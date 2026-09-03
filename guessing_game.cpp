25031be131a801b50246b4e6943b1ce095568ab6
vote4u <noobyt30k@gmail.com>
2026-09-03 10:10:23 -0700

    full, working version

--- guessing_game.cpp
+++ guessing_game.cpp
@@ -7,34 +7,36 @@ using namespace std;
    3. Include <iostream>
 */
 int main(){
-  int guess = 0;
-  bool playing = true;
+  int guess = 0;    // initialize user guess
+  bool playing = true;    // initialize playing the game
   while (playing) {
+    cout << "Welcome to Guessing Game. Enter a guess to begin" << endl;
     srand(time(NULL));
-    int number = rand() % 101;
-    int guesses = 0;
-    cin >> guess;
+    int number = rand() % 101;    // init the number to be guessed
+    int guesses = 0;    // initialize number of guess
+    cin >> guess;    // take guess
+    
     while (guess != number){
-      guesses+=1;
-      if (guess > number){
+      guesses+=1; // add 1 guess for every guess
+      if (guess > number){  // print guesses
         cout << "Too high. " << guesses << " guesses so far." << endl;
       }
       else {
-       cout << "Too low. " << guesses << "guesses so far." << endl;
+       cout << "Too low. " << guesses << " guesses so far." << endl;
       }
       cin >> guess;
     }
-    char play_again = 'n';
-    cout << "You won! Would you like to play again? (y/n)" << endl;
-   
+    char play_again = 'n'; // init if user wants to play again
+    cout << "You won! You took " << guesses << " guesses" << endl;
+    cout << "Would you like to play again? (y/n)" << endl; // ask
     cin >> play_again;
-    if (play_again == 'y'){
-        playing = true;
-        srand(time(NULL));
-        number = rand() % 101;
+    if (play_again == 'y'){ // if they want to play again
+      playing = true; 
+      srand(time(NULL)); // reset number to be guessed
+      number = rand() % 101;
     }
     else{
-    playing = false;
+      playing = false; // no play
     }
   }
   return 0;
