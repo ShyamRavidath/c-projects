a4cde5de144053d116de8f95b4da7bded10814f3
U-BSD\467905 <467905@SHS-CYBF6X3.beaverton.k12.or.us>
2026-09-18 10:13:20 -0700

    working palindrome game (w/o punctuation)

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,27 +1,52 @@
 #include <iostream>
 #include <cstring>
+#include <cctype>
 
 using namespace std;
 
 int main()
 {
-  char str[20];
-  char str2[20];
-  cin.get(str, 20);
-  cin.get();
-  cin.get(str2, 20);
+  char str[80];
+  char str2[80];
+  cin.get(str, 80);
   cin.get();
   int count = 0;
+  int len = strlen(str);
   
-  for (int i = 0; i<20; i++){
-    if (str[i] = '\0'){ 
-      str2[count] = str[i];
+  for (int i = len; i > 0; i--){
+    if (ispunct(str[i-1]) == 0 && str[i-1] != ' '){ 
+      if (str[i-1] != '\0'){
+	cout << str[i-1] << endl;
+	str2[count] = str[i-1];
 	count++;
-    } 
+    }
+    }
   }
+
+  int ct = 0;
+
+  // po.op
+  // p = p
+  // o = o
+  // 
+  for (int i = 0; i < len; i++){ //check if now, each char in reversed spot, is the same
+    if (ispunct(str[i]) == 0 && str[i] != ' '){
+      if (str2[i] == str[i]){
+	ct++;
+      }
+    }
+    
+  }
+
+  if (ct != len){ // if any one char is not the same ct != len
+    cout << "Not a palindrome" << endl;
+  }
+  else {
+    cout << "Palindrome" << endl;
+  }  
+  cout << len << endl; 
   
   cout << str << endl;
   cout << str2 << endl;
   return 0;
-
 }
